43c86a4d12c837a13b102197bcc2c761bb613553
Yuanhy1997 <yuanhy1997@foxmail.com>
2023-11-28 15:57:15 +0800

    update

--- inference/inference.sh
+++ inference/inference.sh
@@ -5,7 +5,7 @@ OUTPUT_DIR=$3
 NUM_OF_PROCESS=$4
 NUM_OF_PROCESS=${NUM_OF_PROCESS:-8}
 
-torchrun --nproc-per-node=$NUM_OF_PROCESS inference.py \
+torchrun --nproc-per-node=$NUM_OF_PROCESS --rdzv-backend=c10d inference.py \
         --model-path $MODEL_PATH \
         --model-id $MODEL_PATH \
         --max-new-token 2048 \
