fcaed566809603e37720804d3c4dd0bdbb6d105f
Yuanhy1997 <yuanhy1997@foxmail.com>
2023-11-29 01:34:19 +0800

    update

---
+++ fine-tune/llama2_train_across_node.sh
@@ -0,0 +1,29 @@
+MODEL_PATH=$1
+DATA_FILE=$2
+SAVE_PATH=$3
+
+wandb disabled
+torchrun --nproc_per_node=8 --master_port=$MASTER_PORT --nnode 2 --node_rank=$RANK main_llama2.py \
+    --model_name_or_path $MODEL_PATH \
+    --data_path $DATA_FILE \
+    --bf16 True \
+    --output_dir output/$SAVE_PATH \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --deepspeed ./deepspeed_configs/zero3.json \
+    # --fsdp "full_shard auto_wrap" \
+    # --fsdp_config ./fsdp_configs/llama-7b-config.json \
+
+
