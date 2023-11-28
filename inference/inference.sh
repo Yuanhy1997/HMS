dd6bd61609b60c753e4cc618623c25c768eb0641
Yuanhy1997 <yuanhy1997@foxmail.com>
2023-11-28 17:03:48 +0800

    update

--- inference/inference.sh
+++ inference/inference.sh
@@ -5,10 +5,20 @@ OUTPUT_DIR=$3
 NUM_OF_PROCESS=$4
 NUM_OF_PROCESS=${NUM_OF_PROCESS:-8}
 
-torchrun --nproc-per-node=$NUM_OF_PROCESS --rdzv-backend=c10d inference.py \
-        --model-path $MODEL_PATH \
-        --model-id $MODEL_PATH \
-        --max-new-token 2048 \
-        --temperature 0.7 \
-        --question-file $INPUT_FILE \
-        --answer-file output/$OUTPUT_DIR/results.jsonl \
+# torchrun --nproc-per-node=$NUM_OF_PROCESS --rdzv-backend=c10d inference.py \
+#         --model-path $MODEL_PATH \
+#         --model-id $MODEL_PATH \
+#         --max-new-token 2048 \
+#         --temperature 0.7 \
+#         --question-file $INPUT_FILE \
+#         --answer-file output/$OUTPUT_DIR/results.jsonl \
+
+if [ $NUM_OF_PROCESS -gt 1 ];  
+then
+     files=(output/$OUTPUT_DIR/results_*)
+     for f in ${files[@]}
+     do
+        echo $f
+        cat $f >> output/$OUTPUT_DIR/results.jsonl
+     done
+fi
