20b0ebe08233a5b0e1ba0719ab12deb01d6877f5
Yuanhy1997 <yuanhy1997@foxmail.com>
2023-11-30 12:13:56 +0800

    update

---
+++ fine-tune/main_llama_mnode_70b.sh
@@ -0,0 +1,28 @@
+MODEL_PATH=$1
+DATA_FILE=$2
+SAVE_PATH=$3
+
+wandb disabled
+torchrun --nproc_per_node=8 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --nnode 3 --node_rank=$RANK train.py \
+    --model_name_or_path $MODEL_PATH \
+    --data_path $DATA_FILE \
+    --fp16 True \
+    --output_dir output/$SAVE_PATH \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 10 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --deepspeed ./deepspeed_configs/zero3.json \
+    # --fsdp "full_shard auto_wrap" \
+    # --fsdp_config ./fsdp_configs/llama-30b-config.json \
+   
